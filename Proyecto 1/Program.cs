ca2fa67196a52ed04900af80f03e1a53b957f4dd
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-16 07:05:15 -0600

    declaracion de variables

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Simulador de decisiones para plataforma de streaming");
+
+int publicados = 0;
+int rechazados = 0;
+int revision = 0;
+int altoImpacto = 0;
+int impactoMedio = 0;
+int bajoImpacto = 0;
+
