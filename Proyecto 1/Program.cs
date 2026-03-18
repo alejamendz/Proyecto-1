79822bde70eb908264449e7947d21fb6e2baa514
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-18 13:01:13 -0600

    Creacion del menú

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -8,3 +8,21 @@ int altoImpacto = 0;
 int impactoMedio = 0;
 int bajoImpacto = 0;
 
+int opcion = 0;
+do
+{
+    Console.WriteLine("Menú principal");
+    Console.WriteLine("1. Evaluar nuevo contenido");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión");
+    Console.WriteLine("4. Reiniciar estadísticas");
+    Console.WriteLine("5. Salir");
+
+    Console.WriteLine("Seleccione una opcion del menú");
+    opcion =int.Parse(Console.ReadLine());
+
+
+
+
+
+} while (opcion != 5);
